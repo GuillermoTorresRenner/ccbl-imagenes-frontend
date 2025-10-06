4f84720826abf311a6c3b9bebf06b1bf2dbcd4a6
Guillermo Torres <torresrennerguilermo@gmai.com>
2025-10-06 12:44:44 -0300

    init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,10 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../components/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Lo Barnechea en Imágenes" },
+    { name: "description", content: "Explora imágenes de Lo Barnechea" },
   ];
 }
 
